5a62d2b4d452a8b6779e0159086af2d9dd0adfd7
Ioana Seritan <ioana@campus-051-226.ucdavis.edu>
2024-10-03 13:55:47 -0700

    learning functions and elephants in class

---
+++ Week2_IntroToR.R
@@ -0,0 +1,23 @@
+3+4
+3 + 4
+2 *
+4+8*3^2
+4 + 8 * 3 ^ 2  
+4+8*3^2
+?log
+
+x <- 1
+rm(x)
+sqrt(exp(4))
+mynumber <- 6
+mynumber == 5
+mynumber != 5
+mynumber > 4
+
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+elephant1_lb <- elephant1_kg * 2.2
+elephant2_lb > elephant1_lb
+myelephants <- c(elephant1_lb, elephant2_lb)
+myelephants
+which(myelephants == max (myelephants))
